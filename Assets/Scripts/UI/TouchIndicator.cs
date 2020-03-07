2e8a91e1045e741efea2408207e310520c80ec57
sebit0nic <sebastian.lackner7@gmail.com>
2020-03-07 23:42:32 +0100

    Refined some icons, added some basics for the tutorial

---
+++ Assets/Scripts/UI/TouchIndicator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchIndicator : MonoBehaviour {
+
+    public float minTouchdownTime;
+
+    private RectTransform fillLevelTransform;
+
+    private void Start() {
+        fillLevelTransform = transform.Find("Fill Level").GetComponent<RectTransform>();
+    }
+}
